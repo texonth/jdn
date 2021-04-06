0489752eea15112b431743d19af11d6bcdb3a053
Allin Nikita (Allen Wahlberg) <allin.nikita@yandex.ru>
2021-04-06 13:48:24 +0300

    redesign (#91)
    Co-authored-by: Allin Nikita <Nikita_Allin@epam.com>

--- src/js/blocks/generate/GenerateBlock.jsx
+++ src/js/blocks/generate/GenerateBlock.jsx
@@ -6,25 +6,9 @@ import { Button } from "antd";
 import { exportIcon, importIcon, settings } from "../../../icons";
 import ReactFileReader from "react-file-reader";
 
-const styles = {
-  generateStyle: {
-    margin: "10px 0 10px 10px",
-  },
-  generateStyleAll: {
-    margin: "10px 0 10px 10px",
-    display: "flex",
-  },
-};
-
 @inject("mainModel")
 @observer
-class GenerateBlock extends React.Component {
-  handleGenerate = () => {
-    const { mainModel } = this.props;
-
-    mainModel.generateBlockModel.generate(mainModel);
-  };
-
+export default class GenerateBlock extends React.Component {
   handleGenerateSeveral = () => {
     const { mainModel } = this.props;
 
@@ -49,52 +33,47 @@ class GenerateBlock extends React.Component {
 
   render() {
     const { classes, mainModel } = this.props;
-    const isEnabled = (mainModel.generateBlockModel.urlsList || []).length;
+    const urlsList = mainModel.generateBlockModel.urlsList;
+    const isEnabled = (urlsList || []).length;
+
 
     return (
       <div>
         <div>
-          <div className={`${classes.generateStyle} BtnGroup`}>
-            <Button size={"small"} onClick={this.handleGenerate}>
-              Generate
-            </Button>
-            <Button size={"small"} onClick={this.handleOpenSettings}></Button>
-          </div>
-        </div>
-        <div>
-          <div className={`${classes.generateStyleAll}`}>
+          <div className={`btnGroup`}>
             <Button
+              className={`inlineBtn`}
               size={"small"}
               disabled={!isEnabled}
               onClick={this.handleGenerateSeveral}
             >
               Generate Several Pages
             </Button>
-          </div>
-          <div className={`${classes.generateStyleAll}`}>
-            <ReactFileReader
-              handleFiles={(file) => {
-                this.handleImportUrlsListJSON(file);
-              }}
-              fileTypes={[".json"]}
-              multipleFiles={false}
-            >
-              <Button size={"small"}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fill="#464547"
-                    d="M8.25 12.125c.365 0 .674-.128.93-.383l3.937-3.937c.255-.274.383-.588.383-.944 0-.355-.123-.66-.37-.916-.245-.255-.56-.383-.943-.383h-1.421V2.938c0-.364-.128-.674-.383-.93-.255-.254-.565-.382-.93-.382H7.047c-.365 0-.675.128-.93.383s-.383.565-.383.93v2.624H4.313c-.365 0-.67.128-.917.383-.246.256-.373.56-.382.916-.01.356.114.67.369.944l3.937 3.937c.256.255.565.383.93.383zm0-1.313L4.312 6.876h2.735V2.937h2.406v3.938h2.735L8.25 10.813zm4.922 3.063c.091 0 .168-.032.232-.096s.096-.141.096-.232v-.656c0-.092-.032-.169-.096-.233s-.141-.095-.232-.095H3.328c-.091 0-.168.031-.232.095S3 12.8 3 12.891v.656c0 .091.032.168.096.232s.141.096.232.096h9.844z"
-                  />
-                </svg>
-                Load URLs
-              </Button>
-            </ReactFileReader>
-            <Button size={"small"} onClick={this.handleExportUrlsListJSON}>
+            <div className={`inlineBtn`}>
+              <ReactFileReader
+                handleFiles={(file) => {
+                  this.handleImportUrlsListJSON(file);
+                }}
+                fileTypes={[".json"]}
+                multipleFiles={false}
+              >
+                <Button size={"small"}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill="#464547"
+                      d="M8.25 12.125c.365 0 .674-.128.93-.383l3.937-3.937c.255-.274.383-.588.383-.944 0-.355-.123-.66-.37-.916-.245-.255-.56-.383-.943-.383h-1.421V2.938c0-.364-.128-.674-.383-.93-.255-.254-.565-.382-.93-.382H7.047c-.365 0-.675.128-.93.383s-.383.565-.383.93v2.624H4.313c-.365 0-.67.128-.917.383-.246.256-.373.56-.382.916-.01.356.114.67.369.944l3.937 3.937c.256.255.565.383.93.383zm0-1.313L4.312 6.876h2.735V2.937h2.406v3.938h2.735L8.25 10.813zm4.922 3.063c.091 0 .168-.032.232-.096s.096-.141.096-.232v-.656c0-.092-.032-.169-.096-.233s-.141-.095-.232-.095H3.328c-.091 0-.168.031-.232.095S3 12.8 3 12.891v.656c0 .091.032.168.096.232s.141.096.232.096h9.844z"
+                    />
+                  </svg>
+                  Load URLs
+                </Button>
+              </ReactFileReader>
+            </div>
+            <Button className={`inlineBtn`} size={"small"} onClick={this.handleExportUrlsListJSON}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
@@ -109,14 +88,11 @@ class GenerateBlock extends React.Component {
               Export URLs
             </Button>
           </div>
+          {urlsList.map((url) => (
+            <a>{url}</a>
+          ))}
         </div>
       </div>
     );
   }
 }
-
-GenerateBlock.propTypes = {};
-
-const GenerateBlockWrapper = injectSheet(styles)(GenerateBlock);
-
-export default GenerateBlockWrapper;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import injectSheet from "react-jss";
-import { observer, Provider } from "mobx-react";
+import {inject, observer, Provider} from "mobx-react";
 import {computed, observable} from "mobx";
 
 import GenerateResultsWrapper from "./blocks/generate/GenerateResults";
 import GeneralSettingsWrapper from "./blocks/generate/GeneralSettings";
 import { RulesBlock } from "./blocks/rules/RulesBlock";
 
-import GenerateBlockWrapper from "./blocks/generate/GenerateBlock";
+import GenerateBlock from "./blocks/generate/GenerateBlock";
 
 import MainModel from "./models/MainModel";
 
@@ -18,6 +18,7 @@ const { SubMenu } = Menu;
 import "antd/lib/style/themes/default.less";
 import "antd/dist/antd.less";
 import "../css/main.less";
+import LogComponentWrapper from "./blocks/log/LogComponent";
 const styles = {
   commonContainer: {
     position: "relative",
@@ -44,8 +45,14 @@ class App extends React.Component {
   @observable mainModel = new MainModel();
 
   handleClick = (e) => {
+    console.log(e.key);
     this.mainModel.setTab(e.key);
     this.setState({tab: e.key});
+  };
+
+  handleGenerate = (mainModel) => {
+    mainModel.generateBlockModel.generate(mainModel);
+    this.handleClick({key: "results"});
   };
 
   @computed get tab() {
@@ -68,7 +75,7 @@ class App extends React.Component {
             <Menu.Item key="urls">URLs</Menu.Item>
             <Menu.Item key="results">Results</Menu.Item>
             <Menu.Item disabled={true}>
-              <Button size={"small"} type="primary">
+              <Button  onClick={() => {this.handleGenerate(this.mainModel)}} size={"small"} type="primary">
                 GENERATE
               </Button>
             </Menu.Item>
@@ -97,45 +104,22 @@ class App extends React.Component {
 
           {this.tab === "urls" && (
             <div key="urls">
-              <GenerateBlockWrapper></GenerateBlockWrapper>
-              <GenerateResultsWrapper></GenerateResultsWrapper>
+              <GenerateBlock></GenerateBlock>
             </div>
           )}
 
           {this.tab === "results" && (
             <div key="results">
-              {/*<ListOfSearchAttributesWrapper></ListOfSearchAttributesWrapper>*/}
-              {/*<GeneralSettingsWrapper></GeneralSettingsWrapper>*/}
               <GenerateResultsWrapper></GenerateResultsWrapper>
             </div>
           )}
 
-          {/*<LogComponentWrapper />*/}
-          {/*{!this.mainModel.showLog && (*/}
-          {/*<div>*/}
-          {/*<TabsBlockWrapper />*/}
-          {/*<MediaQuery minWidth={ScreenSizes.tablet}>*/}
-          {/*<div className={classes.contentContainerDesktopTablet}>*/}
-          {/*<LeftBlockWrapper>*/}
-          {/*<ComponentLeft />*/}
-          {/*</LeftBlockWrapper>*/}
-          {/*<RightBlockWrapper>*/}
-          {/*{ComponentRight && <ComponentRight />}*/}
-          {/*</RightBlockWrapper>*/}
-          {/*</div>*/}
-          {/*</MediaQuery>*/}
-          {/*<MediaQuery maxWidth={ScreenSizes.tablet}>*/}
-          {/*<div className={classes.contentContainerMobile}>*/}
-          {/*<LeftBlockWrapper>*/}
-          {/*<ComponentLeft />*/}
-          {/*</LeftBlockWrapper>*/}
-          {/*<RightBlockWrapper>*/}
-          {/*{ComponentRight && <ComponentRight />}*/}
-          {/*</RightBlockWrapper>*/}
-          {/*</div>*/}
-          {/*</MediaQuery>*/}
-          {/*</div>*/}
-          {/*)}*/}
+          {this.tab === "warnings" && (
+            <div key="warnings">
+              <LogComponentWrapper />
+            </div>
+          )}
+          {this.mainModel.showLog}
         </div>
       </Provider>
     );
