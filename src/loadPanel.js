62d0fc6bcc37d91605c27a35230728f6a970a1aa
Mariia_Nebesnova <Mariia_Nebesnova@epam.com>
2021-04-21 09:35:24 +0300

    replace inline script

--- src/loadPanel.js
+++ src/loadPanel.js
@@ -1,8 +1,5 @@
 window.onload = () => {
-  /*global chrome*/
-  /*eslint no-undef: "error"*/
   chrome.devtools.panels.create("JDN", "icon.png", "panel.html", (panel) => {
     console.log(panel);
   });
-
 };
