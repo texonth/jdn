35b1b9fddedea448ba3cf72314a879dfbe810b80
MariiaNebesnova <mariia_nebesnova@epam.com>
2021-06-04 14:54:52 +0300

    backend ver, styling

--- src/js/blocks/autoFind/AutoFind.jsx
+++ src/js/blocks/autoFind/AutoFind.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
-import { useAutoFind } from "./autoFindProvider/AutoFindProvider";
+import React, { useEffect, useState } from "react";
+import injectSheet from "react-jss";
 import { Slider, Row } from "antd";
+import { useAutoFind } from "./autoFindProvider/AutoFindProvider";
 
 import "./slider.less";
+import Layout, { Content, Footer } from "antd/lib/layout/layout";
+import { styles } from "./styles";
 
 let sliderTimer;
-const AutoFind = () => {
+const AutoFind = ({ classes }) => {
   const [perceptionOutput, setPerceptionOutput] = useState(0.5);
+  const [backendVer, setBackendVer] = useState("");
   const [
     {
       status,
@@ -23,6 +27,19 @@ const AutoFind = () => {
       onChangePerception,
     },
   ] = useAutoFind();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("http:localhost:5000/build", {
+        method: "GET",
+      });
+      const r = await result.json();
+      setBackendVer(r);
+    };
+
+    fetchData();
+  }, []);
+
   const handleGetElements = () => {
     identifyElements();
   };
@@ -58,37 +75,41 @@ const AutoFind = () => {
   };
 
   return (
-    <div>
-      <Row>
-        <button disabled={!allowIdetifyElements} onClick={handleGetElements}>
-          Idetify
-        </button>
-        <button disabled={!allowRemoveElements} onClick={handleRemove}>
-          Remove
-        </button>
-        <button disabled={!allowRemoveElements} onClick={handleGenerate}>
-          Generate And Download
-        </button>
-      </Row>
-      <label>Perception treshold: {perception}</label>
-      <Row>
-        <label>0.0</label>
-        <Slider
-          style={{ width: "80%" }}
-          min={0.0}
-          max={1}
-          step={0.01}
-          onChange={handlePerceptionChange}
-          value={perceptionOutput}
-        />
-        <label>1</label>
-      </Row>
-      <div>{status}</div>
-      <div>{pageElements || 0} found on page.</div>
-      <div>{getPredictedElements()} predicted.</div>
-      <div>{getAvailableElements()} available for generation.</div>
-    </div>
+    <Layout>
+      <Content className={classes.content}>
+        <Row>
+          <button disabled={!allowIdetifyElements} onClick={handleGetElements}>
+            Idetify
+          </button>
+          <button disabled={!allowRemoveElements} onClick={handleRemove}>
+            Remove
+          </button>
+          <button disabled={!allowRemoveElements} onClick={handleGenerate}>
+            Generate And Download
+          </button>
+        </Row>
+        <label>Perception treshold: {perception}</label>
+        <Row>
+          <label>0.0</label>
+          <Slider
+            style={{ width: "80%" }}
+            min={0.0}
+            max={1}
+            step={0.01}
+            onChange={handlePerceptionChange}
+            value={perceptionOutput}
+          />
+          <label>1</label>
+        </Row>
+        <div>{status}</div>
+        <div>{pageElements || 0} found on page.</div>
+        <div>{getPredictedElements()} predicted.</div>
+        <div>{getAvailableElements()} available for generation.</div>
+      </Content>
+      <Footer className={classes.footer}>backend ver. {backendVer}</Footer>
+    </Layout>
   );
 };
 
-export default AutoFind;
+const AutoFindWrapper = injectSheet(styles)(AutoFind);
+export default AutoFindWrapper;
