0a853cea2acef5fe6c28a6ccf1e0644ab2e4dbaa
MariiaNebesnova <mariia_nebesnova@epam.com>
2021-06-30 14:42:37 +0300

    context menu events fix

--- src/js/blocks/autoFind/autoFindProvider/contentScripts/contextMenu/contextmenu.js
+++ src/js/blocks/autoFind/autoFindProvider/contentScripts/contextMenu/contextmenu.js
@@ -341,9 +341,9 @@ export const runContextMenu = () => {
   };
 
   document.oncontextmenu = (event) => {
-    event.preventDefault();
     const highlightTarget = event.target.closest("[jdn-highlight=true]");
     if (highlightTarget) {
+      event.preventDefault();
       contextEvent = event;
       chrome.runtime.sendMessage({
         message: "GET_ELEMENT",
@@ -352,8 +352,15 @@ export const runContextMenu = () => {
     }
   };
 
+  document.addEventListener(
+    "mouseleave",
+    () => elementMenu && elementMenu.hide()
+  );
+
   chrome.runtime.onMessage.addListener(({ message, param }) => {
     if (message === "ELEMENT_DATA") {
+      // element can be undefined in case of outdated event listener (after refresh, for example). better solution is to kill listener, but I can't implement it for now
+      if (!param.element) return;
       predictedElement = param.element;
       types = param.types;
       elementMenu && elementMenu.remove();
