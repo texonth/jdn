36a5660437c4f8a7328dd6d00af88f4348361db9
Фёдор Вяткин <texon.th@gmail.com>
2021-07-07 17:25:24 +0300

    test

--- src/loadPanel.js
+++ src/loadPanel.js
@@ -1,7 +1,6 @@
 window.onload = () => {
   /*global chrome*/
   /*eslint no-undef: "error"*/
-
   chrome.devtools.panels.create("JDN", "icon.png", "panel.html", (panel) => {
     console.log(panel);
   });
